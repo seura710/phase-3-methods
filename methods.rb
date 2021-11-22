2982614680938ecee8f51b8ee9956ec552aff4e2
Harris Kim <seura710@gmail.com>
2021-11-22 17:30:56 -0500

    commit tree d6b86d0a9f8b06000a524853c81475b1ce5de19b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
-# Your code here!
+def greetProgrammer()
+    puts "Hello, programmer!"
+end
+
+greetProgrammer()
+
+
+def greet(name)
+    puts `Hello, ${name}!`
+end
+greet("Naureen")
+
+function greetWithDefault(name = "programmer") {
+    console.log(`Hello, ${name}!`);
+  }
+
+def greetWithDefault(name="programmer")
+    puts `Hello, ${name}!`
+end
+greetWithDefault()
+
+function add(num1, num2) {
+    return num1 + num2;
+  }
+
+def add(num1,num2)
+    return num1 + num2
+end
+add(1,2)
+
+
+def halve(number)
+    if number.class !== "number" return null
+        return number / 2
+    end  
